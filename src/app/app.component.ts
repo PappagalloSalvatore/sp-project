6c0cd3e5618152ebc6cf2e2c8964f0c62d58d865
SalvatorePappagallo <salvatore.pappagallo@intecs.it>
2025-05-18 23:11:58 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'spProject';
+  src = "assets/images/gym-video.mp4";
 }
